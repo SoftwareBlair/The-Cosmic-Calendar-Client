3d315c23ec620776f254ae120ab11dcc5a14ed6a
Brandon Blair <brandon.d.blair@gmail.com>
2016-12-03 16:37:01 -0700

    landing page completed

---
+++ src/js/components/main.js
@@ -0,0 +1,28 @@
+// Collapse Navigation on scrolling
+function collapseNavbar() {
+  if ($('.navbar').offset().top > 50) {
+    $('.navbar-fixed-top').addClass('top-nav-collapse');
+  }
+  else {
+    $('.navbar-fixed-top').removeClass('top-nav-collapse');
+  }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// Page Scrolling
+$(function() {
+  $('a.page-scroll').bind('click', function(e) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    e.preventDefault();
+  });
+});
+
+// Closes Menu on click
+$('.navbar-collapse ul li a').click(function() {
+  $(this).closest('.collapse').collapse('toggle');
+});
