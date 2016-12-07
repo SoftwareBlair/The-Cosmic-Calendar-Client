f439a39a0ce7ac8fbc06dd0cfbaf426e148f94d9
Brandon Blair <brandon.d.blair@gmail.com>
2016-12-06 18:47:10 -0700

    adding more functionality and styling to months section

--- src/js/components/months/months.controller.js
+++ src/js/components/months/months.controller.js
@@ -6,32 +6,33 @@
     .module('cosmicCalendar.components.months', [])
     .controller('monthsController', monthsController);
 
-  monthsController.$inject = ['$scope', 'calendarService'];
+  monthsController.$inject = ['calendarService'];
 
-  function monthsController($scope, calendarService) {
+  function monthsController(calendarService) {
     /*jshint validthis: true */
     calendarService.getAllMonths()
     .then(months => {
-      this.allMonths = months.data.data;
+      this.allMonths = months;
       this.currentDaysMonth = null;
       this.currentMonth = null;
+      this.monthHasEvents = false;
+      this.dayHasEvents = false;
 
-      this.getMonth = function (month) {
-        console.log(month);
+      this.getMonth = (month) => {
         let monthID = parseInt(month);
-        this.allMonths.forEach((data) => {
+        this.allMonths.forEach(data => {
           if (monthID === data.id) {
             this.currentMonth = data.month;
             this.currentDaysMonth = new Array(data.num_days);
           }
         });
-        calendarService.getMonthEvents(monthID)
-        .then((event) => {
-          let monthEvents = event.data.data;
-          let daysMonth = this.currentDaysMonth;
 
-          this.getEvent = function (day) {
-            monthEvents.forEach((data) => {
+        calendarService.getMonthEvents(monthID)
+        .then(event => {
+          let monthEvents = event;
+
+          this.getEvent = (day) => {
+            monthEvents.forEach(data => {
               if (day === data.day) {
                 console.log(data);
               }
@@ -41,16 +42,6 @@
       };
     })
     .catch(err => err);
-
-    // calendarService.getAllEvents()
-    // .then((events) => {
-    //   this.allEvents = events.data.data;
-    //
-    //   this.getEvent = function (event) {
-    //     event.forEach(data => console.log(data));
-    //   };
-    // })
-    // .catch(err => err);
 
   }
 
